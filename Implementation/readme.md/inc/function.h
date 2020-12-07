d7ab72a97ffc9baee8a5e8848cbfb25d5b650b6a
99003185 <75428163+99003185@users.noreply.github.com>
2020-12-06 23:18:08 -0800

    Create function.h

---
+++ Implementation/readme.md/inc/function.h
@@ -0,0 +1,18 @@
+#ifndef __FUNCTION_H__
+#define __FUNCTION_H__
+
+#include <stdio.h>
+
+#include<math.h>
+int add(int input1,int input2);
+int subtract(int input1,int input2);
+int multiply(int input1,int input2);
+int divide(int input1,int input2);
+double squareroot(double triginput);
+int modulus(int input1,int input2);
+int fact(int input1);
+double cosine(double triginput);
+double sine(double triginput);
+int power(int input1,int input2);
+
+#endif
